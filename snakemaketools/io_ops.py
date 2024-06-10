aecd799f0474541a5f59d257e2161a00d3c0298a
matteo <matteo.lacki@gmail.com>
2024-06-10 13:47:40 +0200

    not complaining about configs that did not contain a wildcard

--- snakemaketools/io_ops.py
+++ snakemaketools/io_ops.py
@@ -26,12 +26,10 @@ def string_cast(x: str) -> bool | int | float | str:
 def update_wildcards(wildcards: dict, wildcard_diffs: dict) -> None:
     """Dict update but only for existing entries."""
     for wildcard_name in wildcard_diffs:
-        assert (
-            wildcard_name in wildcards
-        ), f"YOU ARE TRYING TO UPDATE A NONEXISTING (FORWARD) WILDCARD {wildcard_name}"
-        print(
-            f"updating wildcard '{wildcard_name}'.\ndefault={wildcards[wildcard_name]}\nnew={wildcard_diffs[wildcard_name]}\n"
-        )
+        if wildcard_name in wildcards:
+            print(
+                f"updating wildcard '{wildcard_name}'.\ndefault={wildcards[wildcard_name]}\nnew={wildcard_diffs[wildcard_name]}\n"
+            )
         wildcards[wildcard_name] = wildcard_diffs[wildcard_name]
 
 
