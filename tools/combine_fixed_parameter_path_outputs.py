54148ff5be92a873798f2b5c364c37c46c47af37
matteo <matteo.lacki@gmail.com>
2024-07-01 12:58:18 +0200

    using args.__dict__

--- tools/combine_fixed_parameter_path_outputs.py
+++ tools/combine_fixed_parameter_path_outputs.py
@@ -70,8 +70,8 @@ def iter_dfs(
 if __name__ == "__main__":
     dfs = list(
         iter_dfs(
-            *args.expandable_bash_paths,
-            _warn_if_missing=not args.strict_on_path_existance,
+            *args["paths"],
+            _warn_if_missing=not args["strict_on_path_existance"],
         )
     )
     if len(dfs) == 0:
@@ -82,10 +82,11 @@ if __name__ == "__main__":
             dfs,
             ignore_index=True,
         )
-        if args.output is None:
+        if args["output"] is None:`
             print(all_stats.to_csv(index=False))
         else:
-            save_df(all_stats, args.output)
+            save_df(all_stats, args["output"])
 # # path = "out/base/{default,_old}/dataset=G8602/calibration=G8605/matches_config=_mz8/fragment_clusters_postprocessing=simple/fragment_clusters_postprocessing_config=default/edge_refinement_config=_ms2_norm_score_geq_{40,50,60,q10} out/base/{default,_old}/dataset=G8602/calibration=G8605/matches_config=_mz8/fragment_clusters_postprocessing=simple/fragment_clusters_postprocessing_config=default/edge_refinement_config=_maxRankLeq{6,8,10,12}"
 # path = "out/base/_old/fragment_stats_config={default,no_normalization,ramintense}/sage/stats.csv"
-# path = "out/base/_old/fragment_stats_config={default,no_normalization,ramintense}/sage/edge_node_counts_summary.csv"
+# path = "out/`base/_old/fragment_stats_config={default,no_normalization,ramintense}/sage/edge_node_counts_summary.csv"
+`
