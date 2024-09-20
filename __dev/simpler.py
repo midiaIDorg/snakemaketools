bf40ab02e5d37ae4b1e070d42be772d597ed9bfc
matteo <matteo.lacki@gmail.com>
2024-09-20 08:21:07 +0200

    seems to work

--- __dev/simpler.py
+++ __dev/simpler.py
@@ -6,108 +6,35 @@ from types import SimpleNamespace
 from typing import Callable
 
 import toml
-# from midia_pipe_hull.pipelines.base import pipeline
+from midia_pipe_hull.pipelines.base import pipeline
 from pony.orm import commit, db_session, set_sql_debug
 from snakemaketools.datastructures import DotDict
-from snakemaketools.models import Path, Rule, db
+from snakemaketools.models import Path, RuleOrConfig, db
 
 set_sql_debug()
-db.bind(provider='sqlite', filename=':memory:', create_db=True)
-# db.bind(provider='sqlite', filename='/home/matteo/Projects/midia/pipelines/devel/midia_pipe/base.sqlite', create_db=True)
+# db.bind(provider='sqlite', filename=':memory:', create_db=True)
+db.bind(provider='sqlite', filename='/home/matteo/Projects/midia/pipelines/devel/midia_pipe/base.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
 
-rule = Rule.GETINSERT(type="raw_data", meta={'path':"spectra/G8027.d", "inputs":{}})
-dataset = Path.GETINSERT(path="spectra/G8027.d", type="raw_data", rule=rule)
-# KURWA
-with db_session:
-    print(dataset.rule)
-# n = Node.GETINSERT(meta=dict(a=10), type="a", path_template="tmp/{id}.dupa")
-
-# n.path
-
-# n = Node(path="A",type="T",_meta="{bla}")
-# m = Node(path="A",type="S",_meta="{bla}")
-# o = Node(path="A",type="S",_meta="{la}")
-# p = Node(type="S",_meta="{zla}")
-# w = Node(path="abaf", type="Z")
-
-# commit()
-# Node.get(_meta="{la}")
-# Node.get(_meta="{bla}")
-# Node.get(path="A")
-# Node.get(path="abaf")
-
-
-
-
-
-
-# OK, this is really awesome: there will be no distinction between the configs made automatically and those we provide.
-
-# def parser(id):
-#     node = Node.GETINSERT(id)
-#     origin = json.loads(node.origin)
-#     return dict(
-#         a = origin.inputs["a"],
-#         b = origin.inputs["b"],
-#         c = origin.inputs["c"],
-#     )
-
-# rule cluster:
-#     input:
-#         unpack(parser)
-#     output: 
-#         "blablalba/{id}.parquet"
-
-# how to proceed now?  
-
-# plan it well: 
-
-# how should one deal with the config -> configs?
-# * a script should simply put all of those configs in proper places while filling the DB about them. No, first fill it in, then copy all in right places.
-# * fill in the DB with other nodes
-# * finally, call Snakemake
-
-# how should the script know what are the configs?
-# Node._origin json can 
-# We need to know which of the thing was filled.
 
 with open("configs/consolidated/default.toml", "r") as f:
     config = toml.load(f)
     # pprint(config)
 
-# we should somehow turn that to None?
-# config["baseline_removal_config"]
-# config["subconfigs"]["precursor_clustering_config"]["config"]
-# config["subconfigs"]["matching_config"]["config"]
-# config["subconfigs"]["precursor_cluster_stats_config"]["config"]
-
 dataset = "G8027"
 calibration = "G8045"
 fasta = "Human_2024_02_16_UniProt_Taxon9606_Reviewed_20434entries_contaminant_tenzer"
 
-
-
-roots = DotDict()
-
-roots["dataset"] = Node.GETINSERT(type="raw_data", path_template=f"spectra/{dataset}.d", meta={})
-roots["dataset_tdf"] = Node.GETINSERT(type="sqlite", path=f"spectra/{dataset}.d/analysis.tdf")
-roots["dataset_tdf_bin"] = Node.GETINSERT(type="tdf", path=f"spectra/{dataset}.d/analysis.tdf_bin")
-
-roots["calibration"] = Node.GETINSERT(type="raw_data", path=f"spectra/{calibration}.d")
-roots["calibration_tdf"] = Node.GETINSERT(type="sqlite", path=f"spectra/{calibration}.d/analysis.tdf")
-roots["calibration_tdf_bin"] = Node.GETINSERT(type="tdf", path=f"spectra/{calibration}.d/analysis.tdf_bin")
-
+paths = pipeline(subconfigs=config["subconfigs"], dataset=dataset, calibration=calibration, fasta=fasta)
+paths["fasta"].path
+paths["dataset"].path
+paths["dataset_analysis_tdf_hash"].path
 # how to pass in calibration=None?
 # how to pass in a fasta?
 
 # subconfig_name, subconfig = next(iter(config["subconfigs"].items()))
-for subconfig_type, subconfig in config["subconfigs"].items():
-    roots[subconfig_type] = Node.GETINSERT(type=subconfig_type, meta=subconfig)
 
 
-graph = pipeline(**roots)
-graph
 
 node_ids = {k: node.id for k, node in graph.items() if node != None}
 node_ids# drop this to a json/toml.
@@ -139,6 +66,9 @@ with
 node.origin["config"]
 
 Node[22].origin
+
+
+# OK, this is really awesome: there will be no distinction between the configs made automatically and those we provide.
 
 
 # should the consolidated config contain any paths or not? 
