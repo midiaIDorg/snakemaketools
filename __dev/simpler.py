fdc4e7583d721080b4bb739c7d211cdfd929dce7
matteo <matteo.lacki@gmail.com>
2025-01-10 10:58:35 +0100

    developping

--- __dev/simpler.py
+++ __dev/simpler.py
@@ -22,22 +22,27 @@ longsnake = LongSnakeConfiguration(
     smk_file_paths=Path("workflow").glob("**/*.smk"),
 )
 diff = "G8027/None"
+# diff = "G8602/None"
 longsnake.update_consolidated_config(diff)
 
 rules = longsnake.rules
 configs = longsnake.configs
 wildcards = longsnake.wildcards
-
-
-longsnake.nodes
-
-
-# this is stupidly repeated
-
 nodes = longsnake.nodes
 
+
+nodes.first_gen_sage_results
+nodes.first_gen_search_precurors
+nodes.first_gen_search_fragments_summary
+
+
+
+nodes.rough_matches_summary
 nodes.refined_matches_stats
 nodes.first_gen_search_fragments
 nodes.remaining_first_gen_edges_counts
 nodes.refined_matches_qc
 nodes.refined_nodes_quality_checks
+
+
+"precursor_cluster_stats", "fragment_cluster_stats", "rough_matches", "first_gen_search_precursors", "first_gen_search_fragments",
