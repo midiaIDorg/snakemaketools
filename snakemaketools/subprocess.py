153c1c5c987ccd99513e34d364e8701a2ec86c7d
Mateusz Lacki <matteo.lacki@gmail.com>
2025-02-26 07:41:24 +0100

    wrapper around subprocess.run  added

---
+++ snakemaketools/subprocess.py
@@ -0,0 +1,27 @@
+import subprocess
+
+
+def run(cmd, *args, capture_output=True, check=True, verbose=True, **kwargs):
+    if verbose:
+        print("running:")
+        print()
+        pprint(cmd)
+        print()
+    try:
+        res = subprocess.run(
+            cmd, *args, capture_output=capture_output, check=check, **kwargs
+        )
+        return res
+    except subprocess.CalledProcessError as exc:
+        print("-----------------")
+        print("SUBPROCESS FAILED")
+        print(f"Return Code: {exc.returncode}")
+        print(f"Command: {exc.cmd}")
+        print()
+        print(f"STDOUT:")
+        print(exc.stdout)
+        print()
+        print(f"STDERR:")
+        print(exc.stderr)
+        print("-----------------")
+        raise exc
