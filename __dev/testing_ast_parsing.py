96cddeea9e98085cb9730e24e52e1e1cdca5982d
Mateusz Lacki <matteo.lacki@gmail.com>
2025-03-13 14:17:09 +0100

    black formatting

--- __dev/testing_ast_parsing.py
+++ __dev/testing_ast_parsing.py
@@ -2,11 +2,13 @@
 %autoreload 2
 
 import pathlib
+
 from collections import defaultdict
 from pathlib import Path
 
 import toml
 
-from snakemaketools.parsers import iter_lines_recursively, simple_toml_finder
+from snakemaketools.parsers import iter_lines_recursively
+from snakemaketools.parsers import simple_toml_finder
 
 tomls = simple_toml_finder(iter_lines_recursively(root="workflow"))
